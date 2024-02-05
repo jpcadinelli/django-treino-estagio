6f7716b632c4f775c99db6eca64d05feb4e16de3
jpcadinelli <jpcadinelli@gmail.com>
2024-02-05 17:49:28 -0300

    final da aula 23

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), # Home
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato), # /contato/
+    path('', include('recipes.urls')),
 ]
