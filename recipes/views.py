f9ef9d21b9d20e67bce6f70fe0e71c855aed92e8
jpcadinelli <jpcadinelli@gmail.com>
2024-02-06 07:45:35 -0300

    aula 24

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
